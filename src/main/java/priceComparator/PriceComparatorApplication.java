76dc341605ba1361409b2d576624fdc3b32e2b1e
IulianCosman <iuliancosman709@gmail.com>
2025-05-09 21:12:23 +0300

    MySql DB connection created, initial Product model class created

--- src/main/java/priceComparator/PriceComparatorApplication.java
+++ src/main/java/priceComparator/PriceComparatorApplication.java
@@ -1,4 +1,4 @@
-package com.example.pricecomparator;
+package priceComparator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
