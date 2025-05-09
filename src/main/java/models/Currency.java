ea344ff83258c27bfa2c63867b85b5833613251b
IulianCosman <iuliancosman709@gmail.com>
2025-05-09 21:12:23 +0300

    MySql DB connection created, initial Product model class created

---
+++ src/main/java/models/Currency.java
@@ -0,0 +1,8 @@
+package models;
+
+public enum Currency {
+    RON,
+    EUR,
+    USD,
+    UNKNOWN
+}
---
+++ src/main/java/models/Product.java
@@ -0,0 +1,37 @@
+package models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String category;
+
+    private String brand;
+
+    private Double packageQuantity;
+
+    private String packageUnit;
+
+    private Double price;
+
+    private Currency currency;
+
+}
